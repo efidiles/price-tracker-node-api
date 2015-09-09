6758814babc6504afe992cde43978d1dd40595c7
Ed <efidiles@gmail.com>
2015-09-09 18:43:22 +0100

    remove test code

--- test/email-sender.spec.js
+++ test/email-sender.spec.js
@@ -1,16 +1,7 @@
-var emailSender = require('../helpers/email-sender')({
-  service: 'Gmail',
-  auth: {
-    user: process.env.NODE_APP_EMAILSENDER_EMAIL,
-    pass: process.env.NODE_APP_EMAILSENDER_PASSWORD
-  }
-});
-
-describe('email-sender.js', function() {
-  it('should load content from url', function() {
-    return emailSender.sendActivationEmail({
-      email: 'efidiles.public@gmail.com',
-      token: 'testtoken-000000-000000000000000'
-    });
-  });
-});
+// var emailSender = require('../helpers/email-sender')({
+//   service: 'Gmail',
+//   auth: {
+//     user: process.env.NODE_APP_EMAILSENDER_EMAIL,
+//     pass: process.env.NODE_APP_EMAILSENDER_PASSWORD
+//   }
+// });

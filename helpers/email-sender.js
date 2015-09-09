3a42e8e4427513bb60abc72999a45b82b7e3de83
Ed <efidiles@gmail.com>
2015-09-09 20:05:00 +0100

    enable es6 feature following node 4.0 release

--- helpers/email-sender.js
+++ helpers/email-sender.js
@@ -5,29 +5,29 @@ var logger = require('./logger').FIDI.forModule(__filename);
 var senderTransport;
 
 /**
- * Sends an activation email. Options parameters expects and email and
- * a token to be set. The function will send an email with an activation url
- * generated based on the token.
- * @param  {object} options
+ * Sends an activation email. The function will send an email with
+ * an activation url generated based on the token.
+ * @param  {string} email
+ * @param  {string} token
  * @return {Promise}
  */
-function sendActivationEmail(options) {
+function sendActivationEmail(email, token) {
   logger.debug('Sending activation email.');
 
-  var activationUrl = createActivationLink(options.token);
+  var activationUrl = createActivationLink(token);
 
   //TODO: make a better template
-  var mailOptions = {
+  var emailOptions = {
     from: process.env.NODE_APP_EMAILSENDER_EMAIL,
-    to: options.email,
+    to: email,
     subject: 'Activate your account',
-    text: 'Click this link to activate your account: ' +
-      activationUrl,
-    html: '<p>Click this button to activate your account:</p>' +
-      '<p><a href="' + activationUrl + '">Activate</a></p>'
+    text: `Paste the following link in your browser to activate
+          your account: ${activationUrl}`,
+    html: `<p>Click the following button to activate your account:</p>
+          <p><a href="${activationUrl}">Activate</a></p>`
   };
 
-  return senderTransport.sendMailAsync(mailOptions);
+  return senderTransport.sendMailAsync(emailOptions);
 }
 /**
  * Sends an email to the user with the current price for an item (located at
@@ -41,18 +41,19 @@ function sendPriceNotification(email, url, price) {
   logger.debug('Sending price notification email.');
 
   //TODO: make a better template
-  var mailOptions = {
+  var emailOptions = {
     from: process.env.NODE_APP_EMAILSENDER_EMAIL,
     to: email,
     subject: 'Buy your item now',
-    text: 'Your item is now selling for a price of: ' + price +
-      '.\nGo to website to buy it: ' + url,
-    html: '<p>Your item is now selling for a price of: <strong>' +
-      price + '</strong></p>' +
-      '<p>Click <a href="' + url + '">here</a> to go to the website.</p>'
+    text: `Your item is now selling for a price of: ${price}  price.\n
+          Go to website to buy it: ${url}`,
+    html: `<p>Your item is now selling for a price of:
+            <strong>${price}</strong>
+          </p>
+          <p>Click <a href="${url}">here</a> to go to the website.</p>`
   };
 
-  return senderTransport.sendMailAsync(mailOptions);
+  return senderTransport.sendMailAsync(emailOptions);
 }
 
 function createActivationLink(token) {
